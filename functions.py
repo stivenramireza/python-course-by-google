3f8f18e302751d67e40cca71c7ae2345b47347ee
stiven <sramir70@eafit.edu.co>
2018-11-28 16:15:49 -0500

    Adding exercises about Strings

---
+++ functions.py
@@ -0,0 +1,21 @@
+# Defines a "repeat" function that takes 2 arguments.
+def repeat(s, exclaim):
+    """
+    Returns the string 's' repeated 3 times.
+    If exclaim is true, add exclamation marks.
+    """
+
+    result = s + s + s # can also use "s * 3" which is faster (Why?)
+    if exclaim:
+        result = result + '!!!'
+    return result
+
+def main():
+    print repeat('Yay', False)      ## YayYayYay
+    print repeat('Woo Hoo', True)   ## Woo HooWoo HooWoo Hoo!!!
+    print 10 * "s"
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    main()
