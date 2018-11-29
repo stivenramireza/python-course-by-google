243dbf23af8db0cedd87f9082a2897ff17b42a48
stiven <sramir70@eafit.edu.co>
2018-11-29 18:05:36 -0500

    Adding solution to first_method() in Dicts and Files exercise

--- Dicts_And_Files/wordcount.py
+++ Dicts_And_Files/wordcount.py
@@ -41,17 +41,39 @@ import sys
 
 # +++your code here+++
 # Define print_words(filename) and print_top(filename) functions.
+def print_words(filename):
+  dict = readFile(filename)
+  for k, v in sorted(dict.items()): print k + " " + str(v)
+
+def print_top(filename):
+  return
+
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 
+def readFile(filename):
+  dictionary = {}
+  lista = []
+  counter = 0
+  f = open('small.txt', 'rU')
+  for line in f:
+    line = line.split()
+    for i in line:
+      lista.append(i)
+  lista2 = [x.lower() for x in lista]
+  for item in lista2:
+    dictionary[item] = dictionary.get(item, 0) + 1
+  del dictionary['--']
+  f.close()
+  return dictionary
 ###
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
   if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
+    print 'usage: ./wordcount.py {--count | --topcount | --read} file'
     sys.exit(1)
 
   option = sys.argv[1]
@@ -60,6 +82,8 @@ def main():
     print_words(filename)
   elif option == '--topcount':
     print_top(filename)
+  elif option == '--read':
+    readFile(filename)
   else:
     print 'unknown option: ' + option
     sys.exit(1)
