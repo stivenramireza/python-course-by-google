d8791a2159f236cd92d3a29e7c76c54fca0304f4
stiven <sramir70@eafit.edu.co>
2018-12-13 22:40:57 -0500

    Adding Google's solution to the optional exercises (Dicts and Files)

--- Dicts_And_Files/mimic.py
+++ Dicts_And_Files/mimic.py
@@ -47,41 +47,38 @@ import sys
 
 def mimic_dict(filename):
   """Returns mimic dict mapping each word to list of words which follow it."""
-  # +++your code here+++
-  return
+  mimic_dict = {}
+  f = open(filename, 'r')
+  text = f.read()
+  f.close()
+  words = text.split()
+  prev = ''
+  for word in words:
+    if not prev in mimic_dict:
+      mimic_dict[prev] = [word]
+    else:
+      mimic_dict[prev].append(word)
+    prev = word
+  return mimic_dict
 
 
 def print_mimic(mimic_dict, word):
   """Given mimic dict and start word, prints 200 random words."""
-  # +++your code here+++
-  return
-
-def readFile(filename):
-  dictionary = {}
-  lista = []
-  counter = 0
-  f = open(filename, 'rU')
-  for line in f:
-    line = line.split()
-    for i in line:
-      lista.append(i)
-  lista2 = [x.lower() for x in lista]
-  for item in lista2:
-    dictionary[item] = dictionary.get(item, 0) + 1
-  del dictionary['--']
-  f.close()
-  return dictionary
+  for i in range(200):
+    print word,
+    nexts = mimic_dict.get(word)
+    if not nexts:
+      nexts = mimic_dict['']
+    word = random.choice(nexts)
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
   if len(sys.argv) != 2:
     print 'usage: ./mimic.py file-to-read'
     sys.exit(1)
-  dict = readFile(sys.argv[1])
-  for k, v in sorted(dict.items()): 
-    print k, v
-  #dict = mimic_dict(sys.argv[1])
-  #print_mimic(dict, '')
+
+  dict = mimic_dict(sys.argv[1])
+  print_mimic(dict, '')
 
 
 if __name__ == '__main__':
